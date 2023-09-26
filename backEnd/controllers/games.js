3fba407caba820e1b9907d75ce8781d6517f4d92
daniel <danielyknechtli@gmail.com>
2023-09-25 17:16:25 -0700

    game review data button

--- backEnd/controllers/games.js
+++ backEnd/controllers/games.js
@@ -17,6 +17,26 @@ router.get("/", async (req, res) => {
     });
 });
 
+//update single game review by id
+router.post("/:id", (req, res) => {
+  console.log("update game");
+  const changes = req.body;
+  const id = req.params.id;
+  db("gameReviews")
+    .where("id", "=", id)
+    .update(changes)
+    .then(() => {
+      console.log("updated game review");
+      return res.redirect("http://localhost:3000/"); //this actual sets the url in the address bar on the client browser to this
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  //db is the name that refers to the postgres software,
+  //postgres is the database software
+  //gameReviews is the name of the database using  postgres software
+});
+
 //return a single game review by id
 router.get("/:id", (req, res) => {
   console.log("return game");
@@ -35,27 +55,6 @@ router.get("/:id", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-});
-
-//update single game review by id
-router.put("/:id", (req, res) => {
-  console.log("update game");
-  const changes = req.body;
-  const id = req.params.id;
-  db("gameReviews")
-    .where("id", "=", id)
-    .update(changes)
-    .then(() => {
-      console.log("update gameReviews");
-      return res.json({ msg: "Update gameReviews" });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.json({ msg: "error occured" });
-    });
-  //db is the name that refers to the postgres software,
-  //postgres is the database software
-  //gameReviews is the name of the database using  postgres software
 });
 
 //delete single game review by id
@@ -81,13 +80,12 @@ router.delete("/:id", (req, res) => {
 router.post("/", async (req, res) => {
   console.log("add one game review");
   const { gameName, gameUrl, gameRating } = req.body;
-  console.log("req.body: " + req.body);
-  const { name } = req.body;
-  console.log("name: " + name);
+  console.log("req.body: " + JSON.stringify(req.body));
   const maxIdQuery = await db("gameReviews")
     .select("id")
     .orderByRaw("id DESC")
     .first();
+    console.log(JSON.stringify(maxIdQuery));
   var maxId = maxIdQuery.id;
   db("gameReviews")
     .insert({
